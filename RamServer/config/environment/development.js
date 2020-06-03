2090a9242bfa569df39f6094b71900af471387cb
Ram kumar <ram.kumar@cybersoft.net>
2020-06-03 14:22:02 +0530

    Removed
    Server code

--- RamServer/config/environment/development.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  DB_URI: 'mongodb+srv://testuser:testuser1@cluster0-qeyz0.mongodb.net/test1?retryWrites=true&w=majority',
-  SESSION_SECRET: 'ddsrerrrresddredfr',
-  JWT_SECRET: 'ddsrerrrresddredfr'
-}
